7fdafeea7155d65c1833b93c8fa9191e5398a042
cguoo0 <cguoo@uw.edu>
2024-04-23 15:52:55 -0700

    init

---
+++ llm_app.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+URL = (
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
+)
+
+
+data = {
+    "contents": [
+        {
+            "parts": [
+                {"text": "Tell me a joke about a fat cat."}
+            ]
+        }
+    ]
+}
+
+res = requests.post(
+    URL,
+    headers={
+        "content-type": "application/json",
+    },
+    json=data,
+    params={"key": os.getenv("GOOGLE_API_KEY")}
+)
+print(res.json())
